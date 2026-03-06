5e58e62e17929a495d8c9effd756a92a34114717
Noel Mier-Luna <noel.lmier@gmail.com>
2026-03-05 21:24:15 -0800

    Final Commit: Linked Lists Modularized!

--- 210-lab-17.cpp
+++ 210-lab-17.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 17 | Noel Mier-Luna
 // Continuing assignment
 #include <iostream>
+
 using namespace std;
 
 // Functions to add
@@ -13,22 +14,20 @@ using namespace std;
 
 const int SIZE = 7;
 
-struct Node
-{
+struct Node {
     float value;
-    Node *next;
+    Node * next;
 };
 
-void output(Node *, float);         // Doesn't need reference since it's only outputting list, not modifying it.
-void addToHead(Node *&, float);     // Passes head by reference so that we can modify it in the function.
-void addToTail(Node *&, float);     // Float parameter is the value to be added to the new node.
-void insertAt(Node *&, float, int); // Int paramter will be used for position to insert/delete.
-void deleteAt(Node *&, float, int);
-void deleteList(Node *&);
+void output(Node * , float); // Doesn't need reference since it's only outputting list, not modifying it.
+void addToHead(Node * & , float); // Passes head by reference so that we can modify it in the function.
+void addToTail(Node * & , float); // Float parameter is the value to be added to the new node.
+void insertAt(Node * & , float, int); // Int paramter will be used for position to insert/delete.
+void deleteAt(Node * & , int);
+void deleteList(Node * & );
 
-int main()
-{
-    Node *head = nullptr;
+int main() {
+    Node * head = nullptr;
     int count = 0;
 
     // create a linked list of size SIZE with random numbers 0-99
@@ -48,7 +47,7 @@ int main()
             newVal->value = tmp_val;
             head = newVal;
         }
-    } 
+    }
     output(head);
 
     // deleting a node
@@ -122,134 +121,113 @@ int main()
     addToHead(head, 10.5);
     addToTail(head, 20.5);
     insertAt(head, 15.5, 2); // Inserts 15.5 at position 2 (between 10.5 and 20.5)
+    addToTail(head, 25.5);
+    deleteAt(head, 2); // Deletes node at position 2 (which is 15.5)
+    deleteList(head); // Deletes the entire list
 
     return 0;
 }
 
-void output(Node *hd)
-{
-    if (!hd)
-    {
+void output(Node * hd) {
+    if (!hd) {
         cout << "Empty list.\n";
         return;
     }
     int count = 1;
-    Node *current = hd;
-    while (current)
-    {
-        cout << "[" << count++ << "] " << current->value << endl;
-        current = current->next;
+    Node * current = hd;
+    while (current) {
+        cout << "[" << count++ << "] " << current -> value << endl;
+        current = current -> next;
     }
     cout << endl;
 }
 
-void addToHead(Node *&head, float value)
-{
-    if (!head)
-    { // if this is the first node, it's the new head
+void addToHead(Node * & head, float value) {
+    if (!head) { // if this is the first node, it's the new head
         head = new Node;
-        head->next = nullptr;
-        head->value = value;
-    }
-    else
-    { // its a second or subsequent node; place at the head
-        Node *newVal = new Node;
-        newVal->next = head;
-        newVal->value = value;
+        head -> next = nullptr;
+        head -> value = value;
+    } else { // its a second or subsequent node; place at the head
+        Node * newVal = new Node;
+        newVal -> next = head;
+        newVal -> value = value;
         head = newVal;
     }
     output(head); // outputs the list after adding the new node to verify that it was added correctly.
 }
 
-void addToTail(Node *&head, float value)
-{
-    if (!head)
-    { // Even if this function is used, checks for empty list.
+void addToTail(Node * & head, float value) {
+    if (!head) { // Even if this function is used, checks for empty list.
         head = new Node;
-        head->next = nullptr;
-        head->value = value;
-    }
-    else
-    { // its a second or subsequent node; place at the tail
-        Node *current = head;
-        while (current->next)
-        { // traverse to the end of the list
-            current = current->next;
+        head -> next = nullptr;
+        head -> value = value;
+    } else { // its a second or subsequent node; place at the tail
+        Node * current = head;
+        while (current -> next) { // traverse to the end of the list
+            current = current -> next;
         }
-        Node *newVal = new Node;
-        newVal->next = nullptr; // new tail node points to null
-        newVal->value = value;
-        current->next = newVal; // old tail node points to new tail node
+        Node * newVal = new Node;
+        newVal -> next = nullptr; // new tail node points to null
+        newVal -> value = value;
+        current -> next = newVal; // old tail node points to new tail node
     }
     output(head); // outputs the list after adding the new node to verify that it was added correctly.
 }
 
-void insertAt(Node *&head, float value, int pos)
-{ // Instead of using cin, we can pass the position as a parameter to the function.
+void insertAt(Node * & head, float value, int pos) { // Instead of using cin, we can pass the position as a parameter to the function.
     cout << "List before inserting " << value << " at position " << pos << ":\n";
     output(head);
-    if (pos == 1)
-    {
+    if (pos == 1) {
         addToHead(head, value); // if the position to insert is 1, we can just call addToHead function
-    }
-    else
-    {
-        Node *current = head; // traverse to the position before where we want to insert
-        for (int i = 1; i < (pos - 1) && current; i++)
-        { // checks for current to be valid to avoid out of bounds.
-            current = current->next;
+    } else {
+        Node * current = head; // traverse to the position before where we want to insert
+        for (int i = 1; i < (pos - 1) && current; i++) { // checks for current to be valid to avoid out of bounds.
+            current = current -> next;
         }
-        if (current)
-        { // if current is valid, we can insert the new node
-            Node *newNode = new Node;
-            newNode->value = value;
-            newNode->next = current->next; // new node points to the next node in the list
-            current->next = newNode;       // current node points to the new node
+        if (current) { // if current is valid, we can insert the new node
+            Node * newNode = new Node;
+            newNode -> value = value;
+            newNode -> next = current -> next; // new node points to the next node in the list
+            current -> next = newNode; // current node points to the new node
         }
     }
     cout << "List after inserting " << value << " at position " << pos << ":\n";
     output(head); // outputs the list after inserting the new node to verify that it was inserted correctly.
 }
 
-void deleteAt(Node *&head, float value, int pos)
+void deleteAt(Node * & head, int pos) // Removed float paramter since the whole node is deleted, including value.
 { // Similar to insertAt, except for deleting node
     cout << "List before deleting node at position " << pos << ":\n";
     output(head);
-    if (pos == 1)
-    {
+    if (pos == 1) {
         deleteList(head); // if the position to delete is 1, we can just call deleteList function to delete the entire list
-    }
-    else
-    {
-        Node *current = head; // Moves to current node to the position before the node we want to delete
-        Node *prev = head; // Keeps track of the previous node to reroute pointers after deleting the current node
-        for (int i = 1; i < (pos - 1) && current; i++)
-        { // checks for current to be valid to avoid out of bounds.
-            current = current->next;
-            prev = prev->next;
+    } else {
+        Node * current = head; // Moves to current node to the position before the node we want to delete
+        Node * prev = head; // Keeps track of the previous node to reroute pointers after deleting the current node
+        for (int i = 1; i < (pos - 1) && current; i++) { // checks for current to be valid to avoid out of bounds.
+            current = current -> next;
+            prev = prev -> next;
         }
-        if (current && current->next) // checks for current and current->next to be valid to avoid out of bounds.
+        if (current && current -> next) // checks for current and current->next to be valid to avoid out of bounds.
         {
-            Node *temp = current->next; // temp node to hold the node we want to delete
-            current->next = temp->next; // current node points to the node after the node we want to delete
-            delete temp;                // deletes the node we want to delete
-            temp = nullptr;             // sets temp to null after deleting the node
+            Node * temp = current -> next; // temp node to hold the node we want to delete
+            current -> next = temp -> next; // current node points to the node after the node we want to delete
+            delete temp; // deletes the node we want to delete
+            temp = nullptr; // sets temp to null after deleting the node
         }
     }
     cout << "List after deleting node at position " << pos << ":\n";
     output(head); // outputs the list after deleting the node to verify that it was deleted correctly
 }
 
-void deleteList(Node *&head)
-{
+void deleteList(Node * & head) {
     cout << "List before deleting the entire list:\n";
     output(head);
-    Node *current = head;
-    while (current)
-    {
-        head = current->next; // moves head to the next node in the list
-        delete current;       // deletes the current node
-        current = head;      // moves current to the new head of the list
+    Node * current = head;
+    while (current) {
+        head = current -> next; // moves head to the next node in the list
+        delete current; // deletes the current node
+        current = head; // moves current to the new head of the list
     }
     head = nullptr; // sets head to null after deleting the entire list
     cout << "List after deleting the entire list:\n";
